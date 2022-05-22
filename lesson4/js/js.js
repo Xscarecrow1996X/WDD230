dd87001109fa6b447058722128ae314266434f60
juandavid_engelbrecht96@hotmail.com <juandavid_engelbrecht96@hotmail.com>
2022-05-21 23:47:46 -0400

    finishing

--- lesson4/js/js.js
+++ lesson4/js/js.js
@@ -1,3 +1,5 @@
+
+// Div that will contain the time at navigation bar
 function datebox() {
   const datefield = document.querySelector(".time-div");
 
@@ -8,117 +10,32 @@ function datebox() {
   return datefield.innerHTML = `<em>${fulldate}</em>`;
 }
 datebox();
-// --------------------------------------------
-// Create button menu 
-function toggleMenu() {
-  document.getElementById("menu").classList.toggle("open");
+//function to toggle the nav bar
+function toggleMenu(){
+  document.querySelector("#primaryNav").classList.toggle("open");
+  document.querySelector("#burguerBtn").classList.toggle("open");
 }
+//add an event listener to the button to call the function toggle menu
+document.querySelector("#burguerBtn").addEventListener("click", toggleMenu);
 
-const x = document.getElementById("burg-btn");
-x.onclick = toggleMenu;
+//get the current date
+const dt = new Date();
 
-// Div that will contain the time at footer
+const year = dt.getFullYear();
 
-const field = document.querySelector(".time");
+document.querySelector("#year").textContent = year;
 
-field.innerHTML = `Last Modification: ${document.lastModified}`;
+let lmodified = document.lastModified;
+document.querySelector("#lmodified").textContent = lmodified;
 
 
-const named = document.getElementById("min-banner");
+const fullDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(dt);
+document.querySelector("#date").innerHTML = `${fullDate}`;
 
-function toggleMinBanner() {
-  named.classList.toggle("show-banner");
+let joinBanner = document.querySelector("#join-banner");
+
+if (dt.getDay() == 1 || dt.getDay() == 2){
+  joinBanner.style.display = "block";
 }
-// --------------------------------------------
-
-const date = new Date();
-const todaysdate = date.getDay();
-if (todaysdate == 1 || todaysdate == 2) {
-  toggleMinBanner();
-  let div = document.querySelector(".show-banner");
-  div.innerHTML = `<p id="text">🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.</p>
-  <button id="btn-p">Join</button>`;
-} else {
-  named.remove();
-}
-// --------------------------------------------
-const paf6 = document.querySelector("#paf6");
-const paf7 = document.querySelector("#paf7");
-const paf8 = document.querySelector("#paf8");
-
-function createElementOne(company) {
-  const h2 = document.createElement("h2");
-  const ul = document.createElement("ul");
-  const p = document.createElement("p");
-  const hr = document.createElement("hr");
-  const p2 = document.createElement("p");
-  const p3 = document.createElement("p");
-  h2.textContent = company.name
-  ul.innerHTML = `<li><img src ="${company.icon}" style="width:90px"></li>`
-  p.textContent = company.frase
-  p2.textContent = company.email
-  p3.textContent = company.phoneNumber
-  hr.style.margin = " 0.3em";
-  paf6.appendChild(h2)
-  paf6.appendChild(ul)
-  paf6.appendChild(p)
-  paf6.appendChild(hr)
-  paf6.appendChild(p2)
-  paf6.appendChild(p3)
-}
-
-function createElementTwo(company) {
-  const h2 = document.createElement("h2");
-  const ul = document.createElement("ul");
-  const p = document.createElement("p");
-  const hr = document.createElement("hr");
-  const p2 = document.createElement("p");
-  const p3 = document.createElement("p");
-  h2.textContent = company.name
-  ul.innerHTML = `<li><img src ="${company.icon}" style="width:90px"></li>`
-  p.textContent = company.frase
-  p2.textContent = company.email
-  p3.textContent = company.phoneNumber
-  hr.style.margin = " 0.3em";
-  paf7.appendChild(h2)
-  paf7.appendChild(ul)
-  paf7.appendChild(p)
-  paf7.appendChild(hr)
-  paf7.appendChild(p2)
-  paf7.appendChild(p3)
-}
-
-function createElementThree(company) {
-  const h2 = document.createElement("h2");
-  const ul = document.createElement("ul");
-  const p = document.createElement("p");
-  const hr = document.createElement("hr");
-  const p2 = document.createElement("p");
-  const p3 = document.createElement("p");
-  h2.textContent = company.name
-  ul.innerHTML = `<li><img src ="${company.icon}" style="width:90px"></li>`
-  p.textContent = company.frase
-  p2.textContent = company.email
-  p3.textContent = company.phoneNumber
-  hr.style.margin = " 0.3em";
-  paf8.appendChild(h2)
-  paf8.appendChild(ul)
-  paf8.appendChild(p)
-  paf8.appendChild(hr)
-  paf8.appendChild(p2)
-  paf8.appendChild(p3)
-}
-
-const jsonUrl = "data/homejson.json"
-
-fetch(jsonUrl)
-  .then((response) => response.json())
-  .then((jsObject_) => {
-      const companies = jsObject_["companies"].at(0);
-      const companiesT = jsObject_["companies"].at(1);
-      const companiesTh = jsObject_["companies"].at(2);
-      console.log(companies)
-      createElementOne(companies);
-      createElementTwo(companiesT)
-      createElementThree(companiesTh)
-  })
+else{
+  joinBanner.style.display = "none";}
