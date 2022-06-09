a66ee40e0e05901db561c07af2c4e9667ec40d9f
juandavid_engelbrecht96@hotmail.com <juandavid_engelbrecht96@hotmail.com>
2022-06-09 18:36:35 -0400

    week8

--- lesson8/table.js
+++ lesson8/table.js
@@ -1,2 +1,2 @@
 const lastmod = document.querySelector("#lastmod");
-lastmod.textContent = `Compiled ${document.lastModified} by Juan David Engelbrecht.`;
+lastmod.textContent = `Compiled ${document.lastModified} by JDavid.`;
