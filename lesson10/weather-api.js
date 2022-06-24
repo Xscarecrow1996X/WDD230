83aef93d81234b06a6d7841b4ec69383cc9775f6
juandavid_engelbrecht96@hotmail.com <juandavid_engelbrecht96@hotmail.com>
2022-06-24 15:35:46 -0400

    week 10

--- lesson10/weather-api.js
+++ lesson10/weather-api.js
@@ -1,18 +1,33 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks,us&units=imperial&appid=3152f5cb63193c20d2712d8adecda329";
 
-fetch(weatherURL)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data); // this is temporary for development only
-    currentTemp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-  const desc = data.weather[0].description;
-  
-  weatherIcon.setAttribute('src', iconsrc);weatherIcon.setAttribute('alt', desc);
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=cbdbf1656275e48900a3931f13929b10";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      //console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function  displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
   captionDesc.textContent = desc;
-  });
-
-  
+}
