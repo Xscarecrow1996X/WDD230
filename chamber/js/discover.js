dd1c2aad4e319a19ae1f6ff48e65341424fc3805
juandavid_engelbrecht96@hotmail.com <juandavid_engelbrecht96@hotmail.com>
2022-07-03 00:18:15 -0400

    week 11

---
+++ chamber/js/discover.js
@@ -0,0 +1,52 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -150px 0px"
+};
+
+
+function preloadImage(image) {
+    const src = image.getAttribute('data-src');
+    if (!src) {
+        return
+    }
+    image.src = src;
+
+}
+
+
+const imgObserver = new IntersectionObserver((entries,
+    imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) { return; } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    });
+}, imgOptions)
+
+imagesToLoad.forEach(image => {
+    imgObserver.observe(image);
+})
+
+
+
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+// Day count & Visit count
+
+const daycount = document.getElementById("number-visits");
